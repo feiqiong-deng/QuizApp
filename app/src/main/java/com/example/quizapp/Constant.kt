8f94ad2149f4fc196becaf1496213ed5f444b7d9
feiqiong-deng <feiqiong.deng@outlook.com>
2021-04-30 14:29:35 -0400

    question UI completed

---
+++ app/src/main/java/com/example/quizapp/Constant.kt
@@ -0,0 +1,47 @@
+package com.example.quizapp
+
+object Constant{
+    fun getQuestions(): ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(
+            1, "What is this?",
+            R.drawable.apple,
+            "Apple", "Banana",
+            "Lemon", "Grape", 1
+        )
+        questionsList.add(que1)
+
+        val que2 = Question(
+                2, "What is this?",
+                R.drawable.banana,
+                "Apple", "Banana",
+                "Lemon", "Grape", 2
+        )
+
+        questionsList.add(que2)
+
+        // 3
+        val que3 = Question(
+                3, "What is this?",
+                R.drawable.sun,
+                "Moon", "Star",
+                "Cloud", "Sun", 4
+        )
+
+        questionsList.add(que3)
+
+        // 4
+        val que4 = Question(
+                4, "What is this?",
+                R.drawable.star,
+                "Moon", "Star",
+                "Cloud", "Sun", 2
+        )
+
+        questionsList.add(que4)
+
+        return questionsList
+    }
+
+}
--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start.setOnClickListener{
+
+            if(inputName.text.toString().isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizQuestions::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
     }
 }
